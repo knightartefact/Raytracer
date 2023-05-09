69e62608da79e21789b4e25b0356e6fb9e644571
Sacha TOPALOFF <sacha.topaloff@epitech.eu>
2023-05-09 16:20:01 +0200

    [FIX] normalize raster sapce coordinates to camera space coordinates (HxW) to (-aRatio, aRatio) x (1, -1)

--- source/Camera.cpp
+++ source/Camera.cpp
@@ -9,17 +9,62 @@
 #include <iostream>
 #include "Camera.hpp"
 
-RayTracer::Camera::Camera(const Math::Point3D &position, double aRatio, int fov) :
-    _screen(Rectangle3D(Math::Point3D(0, 0, 0))),
+RayTracer::CameraScreen::CameraScreen(const Math::Point3D &origin, const Math::Vector3D &botLeft,const Math::Vector3D &topRight) :
+    _origin(origin),
+    _bottomLeft(botLeft),
+    _topRight(topRight)
+{
+}
+
+RayTracer::CameraScreen::CameraScreen(const CameraScreen &rect) :
+    _origin(rect._origin),
+    _bottomLeft(rect._bottomLeft),
+    _topRight(rect._topRight)
+{
+}
+
+RayTracer::CameraScreen &RayTracer::CameraScreen::operator=(const CameraScreen &rect)
+{
+    _origin = rect._origin;
+    _bottomLeft = rect._bottomLeft;
+    _topRight = rect._topRight;
+
+    return *this;
+}
+
+RayTracer::CameraScreen &RayTracer::CameraScreen::operator=(CameraScreen &&rect)
+{
+    _origin = rect._origin;
+    _bottomLeft = rect._bottomLeft;
+    _topRight = rect._topRight;
+
+    return *this;
+}
+
+Math::Point3D RayTracer::CameraScreen::pointAt(double u, double v) const
+{
+    Math::Point3D point;
+
+    point._x = ((2 * u) - 1) * _topRight._x;
+    point._y =  1 - (2 * v);
+    point._z = _origin._z;
+    return point;
+}
+
+RayTracer::Camera::Camera(const Math::Point3D &position, const ImageRes &res, int fov) :
+    _screen(CameraScreen(Math::Point3D(), Math::Vector3D(-1, -1, 0), Math::Vector3D(1, 1, 0))),
     _position(position),
     _direction(Math::Vector3D(0, 0, 0)),
     _fov(fov * (M_PI / 180)),
-    _aspectRatio(aRatio)
+    _resolution(res),
+    _aspectRatio(res.width / (double)res.height)
 {
-    double screen_distance =  1 /  (2 * std::tan(_fov / 2.0));
-    Math::Point3D screenOrigin(position._x - 0.5, position._y - 0.5, position._z - screen_distance);
+    double screen_distance =  1 / (2 * std::tan(_fov / 2.0));
+    Math::Point3D screenOrigin(position._x, position._y, position._z - screen_distance);
+    Math::Vector3D botLeft(-_aspectRatio, -1, _position._z - screen_distance);
+    Math::Vector3D topRight(_aspectRatio, 1, _position._z - screen_distance);
 
-    _screen = Rectangle3D(screenOrigin);
+    _screen = CameraScreen(screenOrigin, botLeft, topRight);
 }
 
 RayTracer::Camera::Camera(const Camera &camera) :
@@ -27,6 +72,7 @@ RayTracer::Camera::Camera(const Camera &camera) :
     _position(camera._position),
     _direction(camera._direction),
     _fov(camera._fov),
+    _resolution(camera._resolution),
     _aspectRatio(camera._aspectRatio)
 {
 }
@@ -37,6 +83,7 @@ RayTracer::Camera &RayTracer::Camera::operator=(const Camera &camera)
     _position = camera._position;
     _direction = camera._direction;
     _fov = camera._fov;
+    _resolution = camera._resolution;
     _aspectRatio = camera._aspectRatio;
 
     return *this;
@@ -44,7 +91,7 @@ RayTracer::Camera &RayTracer::Camera::operator=(const Camera &camera)
 
 RayTracer::Ray RayTracer::Camera::ray(double u, double v) const
 {
-    Math::Point3D screenPos = _screen.pointAt(u * _aspectRatio, v);
+    Math::Point3D screenPos = _screen.pointAt(u, v);
     Math::Vector3D direction = screenPos - _position;
     direction.normalize();
     Ray ray(_position, direction);
--- source/Camera.hpp
+++ source/Camera.hpp
@@ -9,14 +9,37 @@
 #define CAMERA_HPP_
 
 #include "Vector3D.hpp"
-#include "Rectangle3D.hpp"
 #include "Ray.hpp"
 
 namespace RayTracer
 {
+    struct ImageRes {
+        int width;
+        int height;
+    };
+
+    class CameraScreen {
+        public:
+            CameraScreen(const Math::Point3D &origin, const Math::Vector3D &botleft, const Math::Vector3D &topRight);
+            ~CameraScreen() = default;
+
+            CameraScreen(const CameraScreen &rect);
+            CameraScreen(CameraScreen &&rect) = default;
+
+            CameraScreen &operator=(const CameraScreen &rect);
+            CameraScreen &operator=(CameraScreen &&rect);
+
+            Math::Point3D pointAt(double u, double v) const;
+
+        private:
+            Math::Point3D _origin;
+            Math::Vector3D _bottomLeft;
+            Math::Vector3D _topRight;
+    };
+
     class Camera {
         public:
-            Camera(const Math::Point3D &position, double aRatio, int fov);
+            Camera(const Math::Point3D &position, const ImageRes &res, int fov);
             Camera(const Camera &camera);
             ~Camera() = default;
 
@@ -25,10 +48,11 @@ namespace RayTracer
             Ray ray(double u, double v) const;
 
         private:
-            Rectangle3D _screen;
+            CameraScreen _screen;
             Math::Point3D _position;
             Math::Vector3D _direction;
             double _fov;
+            ImageRes _resolution;
             double _aspectRatio;
     };
 }
