024f4c32f994312d093197229d84358c655d6636
Sacha TOPALOFF <sacha.topaloff@epitech.eu>
2023-05-12 16:44:38 +0200

    [FIX] PointLight only lit towards negative axis

--- source/Lights/PointLight.cpp
+++ source/Lights/PointLight.cpp
@@ -28,11 +28,11 @@ double RayTracer::PointLight::distanceAttenuation(const Math::Point3D &point) co
 RayTracer::Color RayTracer::PointLight::shadowAttenuation(const Math::Point3D &point, const Math::Vector3D &normal, const World &world) const
 {
     float bias = 0.0001f;
-    Math::Vector3D lightDirection = Math::Vector3D::normalize(point - _position);
-    RayTracer::Ray shadowRay(point + (normal * bias), -lightDirection);
+    Math::Vector3D lightDirection = (_position - point);
+    RayTracer::Ray shadowRay(point + (normal * bias), Math::Vector3D::normalize(lightDirection));
     RayTracer::ObjectHit shadowHit = world.hit(shadowRay);
 
-    if (shadowHit.cSolution != std::numeric_limits<double>::infinity()) {
+    if (shadowHit.cSolution < lightDirection.length()) {
         return Color(0, 0, 0);
     }
     return _color;
@@ -40,8 +40,8 @@ RayTracer::Color RayTracer::PointLight::shadowAttenuation(const Math::Point3D &p
 
 double RayTracer::PointLight::angleToNormal(const Math::Point3D &point, const Math::Vector3D &normal) const
 {
-    Math::Vector3D lightDirection = Math::Vector3D::normalize(point - _position);
-    double angle = normal.dot(-lightDirection);
+    Math::Vector3D lightDirection = Math::Vector3D::normalize(_position - point);
+    double angle = normal.dot(lightDirection);
 
     return angle;
 }
